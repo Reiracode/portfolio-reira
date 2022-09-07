90c79a874e47911b5180854900ca4c851e76141e
Reiracode <reira.igg@gmail.com>
2022-09-07 16:14:54 +0800

    Updates

---
+++ precache-manifest.19afd0d5d225c545b5e64a01fd5f1a05.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c512387e68d2d55bf6f6296898230ec",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8855d69b75e4727d8519",
+    "url": "/portfolio/static/css/main.65976fb6.chunk.css"
+  },
+  {
+    "revision": "8795bfcb0b3a1f4a1f31",
+    "url": "/portfolio/static/js/2.5e21464c.chunk.js"
+  },
+  {
+    "revision": "8855d69b75e4727d8519",
+    "url": "/portfolio/static/js/main.0940b188.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "fba9300520f0cd356daa7eefaac45c1f",
+    "url": "/portfolio/static/media/Reira_fuji.fba93005.jpeg"
+  },
+  {
+    "revision": "63a869e6eb2364ff75400e80078e205c",
+    "url": "/portfolio/static/media/covid-19.63a869e6.jpg"
+  },
+  {
+    "revision": "e9c4b99632b188240192b4afb6cf38ec",
+    "url": "/portfolio/static/media/maskfinder.e9c4b996.PNG"
+  },
+  {
+    "revision": "6f93602d83286f077600a25d01f55a91",
+    "url": "/portfolio/static/media/mysystem.6f93602d.jpg"
+  },
+  {
+    "revision": "2ac982d920b01375e3d93bdabde0cc0f",
+    "url": "/portfolio/static/media/rehome.2ac982d9.jpeg"
+  }
+]);
